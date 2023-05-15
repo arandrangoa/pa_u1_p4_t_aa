0c51f658c3c56b845bd89d0f0e6d0ab523c0474c
Alex Andrango <arandrangoa@uce.edu.ec>
2023-05-15 16:22:39 -0500

    Tarea 3

---
+++ src/main/java/com/example/demo/PaU1P4TAaApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaU1P4TAaApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaU1P4TAaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Proyecto Tarea 3");
+	}
+
+}
